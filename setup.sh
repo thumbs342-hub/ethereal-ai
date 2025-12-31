aeb0a4727372ceed7294c52d8f740619b27ce72c
thumbs342-hub <thumbs342@gmail.com>
2025-12-31 12:46:00 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# Installation directe dans le bon dossier
-cd /workspace/ComfyUI/custom_nodes || mkdir -p /workspace/ComfyUI/custom_nodes && cd /workspace/ComfyUI/custom_nodes
+
+# Création du dossier
+mkdir -p /workspace/ComfyUI/custom_nodes
+
+# On entre dans le dossier (en minuscules !)
+cd /workspace/ComfyUI/custom_nodes
+
+# Téléchargement de ReActor
 git clone https://github.com/Gourieff/comfyui-reactor-node.git
-echo "Terminé !"
+
+echo "L'installation est terminée !"
